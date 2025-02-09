19431cc7c8d6fef9b73193135aa95beac1614bf9
Dhina17 <dhinalogu@gmail.com>
2025-02-09 17:21:53 +0100

    sm6150-common: Implement torch light control Change-Id: Icd32d1f6aedb55462c9df4d7cc63a2a4c4e4263e
    [RealJohnGalt:] modify for oneplus usage, add toggle switch
    [cyberknight777]: Adapt nodes and SEPolicy for sunny
    
    Change-Id: I0b86fbeefbe3b951c5b9827a06c066dd71ada002
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>
    Signed-off-by: clarencelol <clarencekuiek@proton.me>

--- sm6150.mk
+++ sm6150.mk
@@ -99,6 +99,7 @@ TARGET_BUILD_DEVICE_AS_WEBCAM := true
 $(call inherit-product-if-exists, vendor/xiaomi/miuicamera/config.mk)
 
 $(call soong_config_set,camera,override_format_from_reserved,true)
+$(call soong_config_set,libcameraservice,ext_lib,//$(LOCAL_PATH):libcameraservice_extension.sm6150)
 $(call soong_config_set,camera,package_name,com.xiaomi.sessionparams.clientName)
 
 PRODUCT_PACKAGES += \
